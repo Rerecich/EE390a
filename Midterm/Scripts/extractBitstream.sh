0813d720c467cee1c3616b6276aef9b3fbf21880
Rerecich <srerecich03@gmail.com>
2025-04-02 16:29:56 +0200

    Add .tcl scripts

--- Midterm/Scripts/extractBitstream.sh
+++
@@ -1,2 +0,0 @@
-cp ./Vivado_Midterm/Vivado_Midterm.runs/impl_1/design_1_wrapper.bit CacheFilterConv2D_HW.bit
-cp ./Vivado_Midterm/Vivado_Midterm.gen/sources_1/bd/design_1/hw_handoff/design_1.hwh CacheFilterConv2D_HW.hwh
